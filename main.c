e0dd7834a5794e701276a81308eeb07cbf8a32b8
mon nom <ryan.thouvenel@epitech.eu>
2022-10-15 11:00:54 +0200

    a2

--- main.c
+++ main.c
@@ -6,19 +6,30 @@
 */
 #include "include/my.h"
 
+int count_c(char *str, char c)
+{
+    int count = 0;
+    for (int n = 0; str[n]; n++) {
+        if (str[n] == c || str[n] == c + 32)
+            count++;
+    }
+    return (count);
+}
+
 int main(int ac, char **av)
 {
     int count = 0;
-    if (ac < 3 || av[2][0] < 'a' && av[2][0] > 'z' &&
-        av[2][0] < 'A' && av[2][0] > 'Z')
+    if (ac < 3)
         return (0);
-    for (int n = 0; av[1][n]; n++) {
-        if (av[1][n] == av[2][0] || av[1][n] == av[2][0] + 32)
-            count++;
+    for (int n = 2; av[n] != NULL; n++) {
+        if (av[n][0] >= 'a' && av[n][0] <= 'z' ||
+            av[n][0] >= 'A' && av[n][0] <= 'Z') {
+            write(1, &av[n][0], 1);
+            write(1, ":", 1);
+            count = count_c(av[1], av[n][0]);
+            my_put_nbr(count);
+            write(1, "\n", 1);
+        }
     }
-    write(1, &av[2][0], 1);
-    write(1, ":", 1);
-    my_put_nbr(count);
-    write(1, "\n", 1);
     return (0);
 }
